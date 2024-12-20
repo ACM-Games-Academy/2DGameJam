40666ada3c26a7c67249d085e84838fd1e85a7c7
JackBrettell <jwb3994@my.acm.ac.uk>
2024-12-20 15:41:14 +0000

    Stuff

---
+++ JamGame/Assets/Dev/JackDev/Target.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+
+    public Sprite sprite1;          //bob up    
+    public Sprite sprite2;          //bob down    
+    public Sprite death1;        
+    public Sprite death2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
